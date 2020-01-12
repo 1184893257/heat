fde56e58bacee2e9823735fa13f876fe6daeff93
qiaoyuliu <qiaoyuliu@tencent.com>
2020-01-12 19:47:50 +0800

    ocr 完成

--- ocr.cpp
+++ ocr.cpp
@@ -5,29 +5,53 @@
 using namespace std;
 using namespace cv;
 
-void rotate(Mat& input)
+extern bool ocr_debug;
+#define DEBUG if (ocr_debug)
+
+void rotate(Mat& input, Mat& origin)
 {
-	// 边缘检测减少计算量
-	Mat borders;
-	Canny(input, borders, 30, 200, 3);
-	imshow("Canny", borders);
+	// 边缘检测减少计算量，实验后发现不能进行边缘检测，会缺少信息，算不准旋转角度
+	//Mat borders;
+	//Canny(input, borders, 30, 200, 3);
+	//DEBUG imshow("Canny", borders);
+	Mat borders = input;
 
 	vector<Vec2f> lines;
 	HoughLines(borders, lines, 1, CV_PI / 180, 20);
 
+	DEBUG {
+		float alpha = 1000;
+		for (size_t i = 0; i < lines.size(); i++)
+		{
+			float rho = lines[i][0], theta = lines[i][1];
+			double cs = cos(theta), sn = sin(theta);
+			double x = rho * cs, y = rho * sn;
+			Point pt1(cvRound(x + alpha * (-sn)), cvRound(y + alpha * cs));
+			Point pt2(cvRound(x - alpha * (-sn)), cvRound(y - alpha * cs));
+			line(origin, pt1, pt2, Scalar(0, 0, 255), 1, LINE_AA);
+		}
+		//line(origin, Point(0, 0), Point(400, 400), Scalar(0, 0, 255), 1, LINE_AA);
+		imshow("originCopy", origin);
+	}
+
 	int numLine = 0;
-	float sumAng = 0.0;
+	double sumAng = 0.0;
 	for (size_t i = 0; i < lines.size(); ++i)
 	{
 		float theta = lines[i][1];
-		if (theta<30 * CV_PI / 180 || (CV_PI - theta)<30 * CV_PI / 180)
+		if (theta < 30 * CV_PI / 180)
 		{
 			numLine++;
 			sumAng = sumAng + theta;
 		}
+		else if ((CV_PI - theta) < 30 * CV_PI / 180)
+		{
+			numLine++;
+			sumAng = sumAng + theta - CV_PI;
+		}
 	}
 	//计算出平均倾斜角，anAng为角度制
-	float avAng = (sumAng / numLine) * 180 / CV_PI;
+	float avAng = (float)((sumAng / numLine) * 180 / CV_PI);
 
 	// 获取二维旋转的仿射变换矩阵
 	int height = input.rows;
@@ -50,7 +74,7 @@ void rotate(Mat& input)
 	warpAffine(input, output, m, outSize, INTER_LINEAR + WARP_FILL_OUTLIERS);
 
 	input = output;
-	imshow("rotate", input);
+	DEBUG imshow("rotate", input);
 }
 
 // 测试无缺失旋转，可输入任何图片
@@ -82,42 +106,48 @@ void testRotate(Mat& input)
 	imshow("justRotate", output);
 }
 
-string ocr(const string& picturePath)
+void cutNums(const Mat& input, vector<Mat>& output)
 {
-	cv::Mat image = cv::imread(picturePath, cv::IMREAD_GRAYSCALE);
-
-	Mat image_gry;
-	resize(image, image_gry, Size(image.cols / 6, image.rows / 6));
-
-	//testRotate(image_gry);
-
-	Mat& image_bin = image;
-	threshold(image_gry, image_bin, 240, 255, THRESH_BINARY); // convert to binary image
-	//imshow("image_bin", image_bin);
-
-	Mat& image_ero = image_gry;
-	Mat element = getStructuringElement(MORPH_RECT, Size(2, 2));
-	erode(image_bin, image_ero, element);
-	//imshow("image_ero", image_ero);
-
-	Mat& image_dil = image_bin;
-	element = getStructuringElement(MORPH_RECT, Size(6, 6));
-	dilate(image_ero, image_dil, element);
-	imshow("image_dil", image_dil);
-
-	// 垂直矫正
-	rotate(image_dil);
-
 	vector<vector<Point> > contours_out;
 	vector<Vec4i> hierarchy;
-	findContours(image_dil, contours_out, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_NONE);
+	findContours(input, contours_out, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
 	// re-arrange location according to the real position in the original image 
 	const size_t size = contours_out.size();
 	vector<Rect> num_location;
+	int maxHeight = 0;
 	for (int i = 0; i < contours_out.size(); i++)
 	{
-		num_location.push_back(boundingRect(Mat(contours_out[i])));// 转换为矩形轮廓
+		Rect bound = boundingRect(Mat(contours_out[i]));
+		num_location.push_back(bound);// 转换为矩形轮廓
+		if (bound.height > maxHeight)
+		{
+			maxHeight = bound.height;
+		}
 	}
+
+	DEBUG {
+		Mat dest;
+		cvtColor(input, dest, COLOR_GRAY2RGB);
+		for (auto& rect : num_location)
+		{
+			rectangle(dest, rect, Scalar(0, 0, 255), 1);
+		}
+		imshow("rects", dest);
+	}
+
+	// 过滤高度不对的
+	for (auto it = num_location.begin(); it != num_location.end();)
+	{
+		if ((*it).height < 0.7 * maxHeight)
+		{
+			it = num_location.erase(it);
+		}
+		else
+		{
+			it++;
+		}
+	}
+
 	sort(num_location.begin(), num_location.end(), [](const Rect& a, const Rect& b)
 	{
 		if (a.x < b.x)
@@ -126,7 +156,151 @@ string ocr(const string& picturePath)
 			return false;
 	}); // 重排轮廓信息
 
-	cv::waitKey(0);
+	int tube_num = 0;
+	for (auto& rect : num_location)
+	{
+		output.push_back(input(rect));
 
+		/*char rectnum[10];
+		_itoa_s(tube_num, rectnum, 10);
+		imshow(string(rectnum), output.at(tube_num));
+		tube_num++;*/
+	}
+}
+
+bool Iswhite(const Mat& input, int top, int bottom, int left, int right)
+{
+	for (int i = top; i <= bottom; ++i)
+	{
+		for (int j = left; j <= right; ++j)
+		{
+			auto p = input.ptr(i, j);
+			if (255 == *p)
+				return true;
+		}
+	}
+	return false;
+}
+
+int TubeIdentification(Mat inputmat) // 穿线法判断数码管a、b、c、d、e、f、g、
+{
+	int tube = 0;
+	int tubo_roi[7][4] =
+	{
+		{ inputmat.rows * 0 / 3, inputmat.rows * 1 / 3, inputmat.cols * 1 / 2, inputmat.cols * 1 / 2 }, // a
+		{ inputmat.rows * 1 / 3, inputmat.rows * 1 / 3, inputmat.cols * 2 / 3, inputmat.cols - 1 }, // b
+		{ inputmat.rows * 2 / 3, inputmat.rows * 2 / 3, inputmat.cols * 2 / 3, inputmat.cols - 1 }, // c
+		{ inputmat.rows * 2 / 3, inputmat.rows - 1    , inputmat.cols * 1 / 2, inputmat.cols * 1 / 2 }, // d
+		{ inputmat.rows * 2 / 3, inputmat.rows * 2 / 3, inputmat.cols * 0 / 3, inputmat.cols * 1 / 3 }, // e
+		{ inputmat.rows * 1 / 3, inputmat.rows * 1 / 3, inputmat.cols * 0 / 3, inputmat.cols * 1 / 3 }, // f
+		{ inputmat.rows * 1 / 3, inputmat.rows * 2 / 3, inputmat.cols * 1 / 2, inputmat.cols * 1 / 2 }, // g
+	};
+
+	if (inputmat.rows / inputmat.cols > 2)   // 1 is special, which is much narrower than others
+	{
+		tube = 6;
+	}
+	else
+	{
+		for (int i = 0; i < 7; i++)
+		{
+			if (Iswhite(inputmat, tubo_roi[i][0], tubo_roi[i][1], tubo_roi[i][2], tubo_roi[i][3]))
+				tube = tube + (1 << i);
+		}
+	}
+
+	switch (tube)
+	{
+	case  63: return 0;  break;
+	case   6: return 1;  break;
+	case  91: return 2;  break;
+	case  79: return 3;  break;
+	case 102: return 4;  break;
+	case 109: return 5;  break;
+	case 125: return 6;  break;
+	case   7: return 7;  break;
+	case 127: return 8;  break;
+	case 111: return 9;  break;
+
+	default: return -1;
+	}
+}
+
+typedef struct
+{
+	Size2i dilateSize;
+}OCR_OPTION;
+
+string try_ocr(const string& picturePath, const OCR_OPTION& option)
+{
+	cv::Mat image = cv::imread(picturePath, cv::IMREAD_GRAYSCALE);
+
+	Mat image_gry;
+	resize(image, image_gry, Size(image.cols / 6, image.rows / 6));
+
+	Mat originCopy;
+	DEBUG cvtColor(image_gry, originCopy, COLOR_GRAY2RGB);
+
+	DEBUG testRotate(image_gry);
+
+	Mat& image_bin = image;
+	threshold(image_gry, image_bin, 240, 255, THRESH_BINARY); // convert to binary image
+	DEBUG imshow("image_bin", image_bin);
+
+	Mat& image_ero = image_gry;
+	Mat element = getStructuringElement(MORPH_RECT, Size(2, 2));
+	erode(image_bin, image_ero, element);
+	DEBUG imshow("image_ero", image_ero);
+
+	// 垂直矫正
+	rotate(image_ero, originCopy);
+
+	Mat& image_dil = image_bin;
+	element = getStructuringElement(MORPH_RECT, option.dilateSize);
+	dilate(image_ero, image_dil, element);
+	DEBUG imshow("image_dil", image_dil);
+
+	// 切割
+	vector<Mat> nums;
+	cutNums(image_dil, nums);
+
+	// 穿线法识别数字
+	string result;
+	for (auto& mat : nums)
+	{
+		int value = TubeIdentification(mat);
+		if (value < 0)
+		{
+			DEBUG imshow("error num", mat);
+		}
+		else
+		{
+			result += '0' + value;
+		}
+	}
+
+	DEBUG waitKey(0);
+
+	return result;
+}
+
+string ocr(const string& picturePath)
+{
+	auto options = vector<OCR_OPTION>
+	{
+		{
+			{3, 6}// 宽一点避免5被上下分隔
+		},
+		{
+			{1, 6}// 窄一点避免粘连上冒号
+		}
+	};
+
+	for (auto& option : options)
+	{
+		string ret = try_ocr(picturePath, option);
+		if (ret.length() >= 3)
+			return ret;
+	}
 	return "";
 }
--- ocr_main.cpp
+++ ocr_main.cpp
@@ -1,10 +1,32 @@
 #include <string>
+#include <sstream>
+#include <iostream>
 using namespace std;
 
 string ocr(const string&);
 
+bool ocr_debug = false;
+
 int main()
 {
-	ocr("heater.jpg");
+	if (ocr_debug)
+	{
+		cout << ocr("D:\\temp\\MobileFile\\8.jpg") << endl;
+	}
+	else
+	{
+#if defined(__linux__)
+		cout << ocr("heat/heater.jpg") << endl;
+#else
+		string dir = "D:\\temp\\MobileFile\\";
+		for (int i = 0; i <= 9; ++i)
+		{
+			stringstream path;
+			path << dir << i << ".jpg";
+
+			cout << ocr(path.str()) << endl;
+		}
+#endif
+	}
 	return 0;
 }
