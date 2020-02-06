dc31c6dbc2b2117ec5974992fcadcf008b116a27
qiaoyuliu <qiaoyuliu@tencent.com>
2020-02-06 23:23:42 +0800

    CreateDir

--- signal_handler.cpp
+++ signal_handler.cpp
@@ -48,6 +48,7 @@ void handleSignal(int signum)
 	{
 		auto rot_str = req["rotate"].get<string>();
 		float rot = (float)atof(rot_str.c_str());
+		CreateDir(path_to_root("camera"));
 		string path_to_heat = "camera/snap.jpg";
 		string abs_path = path_to_root(path_to_heat);
 		capture(abs_path);
